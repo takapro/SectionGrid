8c3c701072dc159bed9e6649f687a5a2033db58e
Takanori Hoshi <takanori.hoshi@gmail.com>
2020-06-08 13:42:13 -0700

    Implement SectionGridAdapter

--- app/src/main/java/com/example/sectiongrid/MainActivity.java
+++ app/src/main/java/com/example/sectiongrid/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.sectiongrid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> sectionTitles = new ArrayList<>();
+        List<List<String>> sectionItems = new ArrayList<>();
+        for (int section = 0; section < 10; section++) {
+            sectionTitles.add("Section " + (section + 1));
+            List<String> items = new ArrayList<>();
+            int numItems = (int) (Math.random() * 20 + 1);
+            for (int i = 0; i < numItems; i++) {
+                items.add("Item " + (section + 1) + "-" + (i + 1));
+            }
+            sectionItems.add(items);
+        }
+
+        final SectionGridAdapter adapter = new SectionGridAdapter();
+        adapter.setSections(sectionTitles, sectionItems);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (adapter.getSectionItemIndex(position).length == 1) {
+                    return 3;
+                } else {
+                    return 1;
+                }
+            }
+        });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/sectiongrid/SectionGridAdapter.java
@@ -0,0 +1,99 @@
+package com.example.sectiongrid;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class SectionGridAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<String> sectionTitles;
+    private List<List<String>> sectionItems;
+
+    public void setSections(List<String> sectionTitles, List<List<String>> sectionItems) {
+        this.sectionTitles = sectionTitles;
+        this.sectionItems = sectionItems;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        int count = sectionTitles.size();
+        for (List<String> items : sectionItems) {
+            count += items.size();
+        }
+        return count;
+    }
+
+    public int[] getSectionItemIndex(int position) {
+        for (int section = 0; section < sectionTitles.size(); section++) {
+            if (position == 0) {
+                return new int[] { section };
+            } else if (position <= sectionItems.get(section).size()) {
+                return new int[] { section, position - 1 };
+            }
+            position -= 1 + sectionItems.get(section).size();
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (getSectionItemIndex(position).length == 1) {
+            return R.layout.layout_title;
+        } else {
+            return R.layout.layout_item;
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(viewType, parent, false);
+        if (viewType == R.layout.layout_title) {
+            return new TitleViewHolder(itemView);
+        } else {
+            return new ItemViewHolder(itemView);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        int[] index = getSectionItemIndex(position);
+        if (getSectionItemIndex(position).length == 1) {
+            ((TitleViewHolder) holder).bindSectionTitle(index[0], sectionTitles.get(index[0]));
+        } else {
+            ((ItemViewHolder) holder).bindItem(index[0], index[1], sectionItems.get(index[0]).get(index[1]));
+        }
+    }
+
+    public static class TitleViewHolder extends RecyclerView.ViewHolder {
+        private TextView titleText;
+
+        public TitleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleText = itemView.findViewById(R.id.titleText);
+        }
+
+        public void bindSectionTitle(int section, String title) {
+            titleText.setText(title);
+        }
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+        private TextView itemText;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemText = itemView.findViewById(R.id.itemText);
+        }
+
+        public void bindItem(int section, int index, String item) {
+            itemText.setText(item);
+        }
+    }
+}
